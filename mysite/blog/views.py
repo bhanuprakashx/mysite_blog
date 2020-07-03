d792b9ec9eba96b5d14ea20a75f282149fd79c15
Bhanu Prakash Palakodeti <bhanu8262@gmail.com>
2020-07-03 07:05:20 +0530

    Email sent.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -33,7 +33,8 @@ def post_list(request):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',
                              publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'posts': post})
+    print(post.id)
+    return render(request, 'blog/post/detail.html', {'post': post})
 
 
 def post_share(request, post_id):
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smptp.gmail.com'
 EMAIL_HOST_USER = '....@gmail.com'
